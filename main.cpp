2569381bc03529f36ecba93341a5f20c16958c09
Choo <zhu.thatboy@outlook.com>
2024-03-01 00:05:39 +0800

    fix: support signal connect to signal, close #2

--- main.cpp
+++ main.cpp
@@ -4,38 +4,37 @@
 
 class Test {
 public:
-    OHTOAI_SIGNAL(test_sig, int, int);
-    ohtoai::signal::signal<std::string, double> visible_sig;
+    OHTOAI_SIGNAL(test_sig, std::string, int);
+    OHTOAI_SIGNAL(test_sig2, std::string, int);
+    OHTOAI_SIGNAL(empty_sig);
 };
 
 int main() {
     Test a;
-    a.test_sig.connect([](int a, int b) {
-        std::cout << "test_sig: " << a << " " << b << std::endl;
+    a.empty_sig.connect([&] {
+        std::cout << __LINE__ << " " << "No args func" << std::endl;
+        });
+    a.test_sig.connect([](std::string a, int b) {
+        std::cout << __LINE__ << " " << a << " " << b << std::endl;
     });
 
-    a.test_sig.connect([](int a, int b) {
-        std::cout << "one more sig: " << a << " " << b << std::endl;
+    a.test_sig2.connect([](std::string a, int b) {
+        std::cout << __LINE__ << " " << a << " " << b << std::endl;
         });
 
-    auto cb = [](int a, int b) {
-        std::cout << "will be removed: " << a << " " << b << std::endl;
-    };
-
-    auto id = a.test_sig.connect(cb);
-    a.test_sig(1, 2);
-    a.test_sig.disconnect(id);
-    a.test_sig(3, 4);
+    auto temp_slot = a.test_sig.connect([](std::string a, int b) {
+        std::cout << __LINE__ << " " << "temp lambda: " << a << " " << b << std::endl;
+        });
+    a.test_sig("Test sig", __LINE__);
+    auto sig_id = a.test_sig.connect(a.test_sig2);
+    a.test_sig("Connect sig to sig2", __LINE__);
+    a.test_sig.disconnect(temp_slot);
+    a.test_sig("Disconnect temp slot", __LINE__);
     a.test_sig.block();
-    a.test_sig(5, 6);
+    a.test_sig("Test block sig", __LINE__);
     a.test_sig.block(false);
-    a.test_sig(7, 8);
-
-    a.visible_sig.connect([](std::string a, double b) {
-        std::cout << "visible_sig: " << a << " " << b << std::endl;
-    });
-    a.visible_sig("test", true);
-    a.visible_sig("one more test", 0xcc);
+    a.test_sig("Test unblock sig", __LINE__);
+    a.empty_sig.emit();
 
     return 0;
 }
--- signal.hpp
+++ signal.hpp
@@ -6,6 +6,7 @@
 #include <memory>
 #include <unordered_set>
 #include <atomic>
+#include <variant>
 #include <functional>
 
 namespace ohtoai{
@@ -17,7 +18,6 @@ namespace ohtoai{
             template<typename T, typename ...Params>
             using mem_func_t = void (T::*)(Params...);
 
-            using callback_t = global_func_t<Args...>;
             using slot_func_t = std::function<void(Args...)>;
 
             using slot_t = std::variant<const signal*, std::weak_ptr<slot_func_t>>;
@@ -31,10 +31,10 @@ namespace ohtoai{
                         return std::get<const signal*>(slot) == std::get<const signal*>(_id.slot);
                     }
                     else if (std::holds_alternative<std::weak_ptr<slot_func_t>>(_id.slot)){
-                        return operator==(id.slot.lock());
+                        return operator==(_id.slot.lock());
                     }
                 }
-                bool operator==(const slot_ptr_t& _slot) const {
+                bool operator==(const std::shared_ptr<slot_func_t>& _slot) const {
                     if (!std::holds_alternative<std::weak_ptr<slot_func_t>>(slot))
                         return false;
                     auto& _slot = std::get<std::weak_ptr<slot_func_t>>(slot);
@@ -45,33 +45,41 @@ namespace ohtoai{
                 const slot_t slot;
             };
         public:
-            slot_handle_t connect(callback_t _fun) {
+            slot_handle_t connect(slot_func_t _fun) {
                 auto f = std::make_shared<slot_func_t>(_fun);
                 slot_funcs.insert(f);
                 return f;
             }
+            slot_handle_t connect(const signal& _sig) {
+                const signal* s = &_sig;
+                slot_sigs.insert(s);
+                return s;
+            }
+
             void disconnect(const slot_handle_t& slot_id) {
                 if (std::holds_alternative<const signal*>(slot_id.slot)) {
-                    slot_sigs.erase(slot_sigs.find(std::get<const signal*>()));
+                    slot_sigs.erase(slot_sigs.find(std::get<const signal*>(slot_id.slot)));
                 }
                 else {
-                    slot_funcs.erase(slot_funcs.find(std::get<std::weak_ptr<slot_func_t>>(slot).lock());
+                    slot_funcs.erase(slot_funcs.find(std::get<std::weak_ptr<slot_func_t>>(slot_id.slot).lock()));
                 }
             }
 
-            void emit(Args...args) {
+            void emit(Args...args) const {
                 if (blocked) return;
-                for (auto& p_slot : slot_funcs) {
-                    std::invoke(*p_slot, std::forward<Args>(args)...);
+
+                auto tuple_args = std::forward_as_tuple(std::forward<Args>(args)...);
+                for (const auto& p_slot : slot_funcs) {
+                    std::apply([&](auto&&... args) { std::invoke(*p_slot, std::forward<decltype(args)>(args)...); }, tuple_args);
                 }
-                for (auto& p_slot : slot_sigs) {
-                    std::invoke(*p_slot, std::forward<Args>(args)...);
+                for (const auto& p_slot : slot_sigs) {
+                    std::apply([&](auto&&... args) { std::invoke(*p_slot, std::forward<decltype(args)>(args)...); }, tuple_args);
                 }
             }
                 
 
-            void operator()(Args...args) {
-                return emit(std::forward<Arg>(args)...);
+            void operator()(Args...args) const {
+                return emit(std::forward<Args>(args)...);
             }
 
             void block(bool _block = true) {
